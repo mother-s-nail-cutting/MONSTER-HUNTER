0f477384c921adbfe5108c0ddecf2fa754db0246
take <shirota.goo@gmail.com>
2022-02-07 23:44:29 +0900

    タイトル完成
    文字にアニメーション追加

--- Title.js
+++ Title.js
@@ -12,6 +12,8 @@ window.addEventListener("load", function() {
 
 
 
+//最大
+
 //タイトルとボタン
 this.setTimeout(function() {
   this.document.getElementById("text").classList.remove("active");
@@ -20,3 +22,26 @@ this.setTimeout(function() {
 setTimeout(function() {
   document.getElementById("loading").classList.remove("active");
 }, 5000)
+
+
+const animationTargetElement = document.querySelectorAll(".main");
+
+const texts = animationTargetElement[0].textContent;
+console.log(texts);
+const textsArray = [];
+
+animationTargetElement[0].textContent = "";
+
+//文字列を分割
+for(let i = 0; i < texts.split("").length; i++){
+  //空白は含めない
+  if(texts.split("")[i] === " "){
+    textsArray.push(" ");
+  }else{
+    textsArray.push('<span style="animation-delay: ' + (i * 0.1) + 's;">' + texts.split("")[i] + '</span>');//一文字ずつ遅延
+  }
+}
+//文字列を元に戻す
+for(let i = 0; i < textsArray.length; i++){
+  animationTargetElement[0].innerHTML += textsArray[i];
+}
